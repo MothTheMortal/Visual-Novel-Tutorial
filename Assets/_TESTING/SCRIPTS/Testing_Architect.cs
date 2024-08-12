85a9d95f0e4b67f28de5be24bcb6b2a411803585
moththemortal <mohdmotha687@gmail.com>
2024-08-12 12:24:58 +0800

    Working on Dialogue System & Added the unncessary folders to git ignore.

--- Assets/_TESTING/SCRIPTS/Testing_Architect.cs
+++ Assets/_TESTING/SCRIPTS/Testing_Architect.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DIALOGUE;
 
 namespace TESTING
 {
     public class Testing_Architect : MonoBehaviour
     {
-        private DialogueSystem ds;
-        private TextArchitect architect;
+        DialogueSystem ds;
+        TextArchitect architect;
+        
+        public TextArchitect.BuildMethod bm = TextArchitect.BuildMethod.instant;
+        
+        
+        
         string[] lines = new string[5]
         {
             "Moth-senpai's curves were as captivating as the sunset, making my heart race uncontrollably.",
@@ -23,13 +29,24 @@ namespace TESTING
         {
             ds = DialogueSystem.instance;
             architect = new TextArchitect(ds.dialogueContainer.dialogueText);
-            
-            
+            architect.buildMethod = TextArchitect.BuildMethod.fade;
+            architect.speed = 0.5f;
+
+
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (bm != architect.buildMethod)
+            {
+                architect.buildMethod = bm;
+                architect.Stop();
+            }
+            
+            if (Input.GetKeyDown(KeyCode.S))
+                architect.Stop();
+            
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 if (architect.isBuilding)
@@ -38,9 +55,11 @@ namespace TESTING
                         architect.hurryUp = true;
                     else
                         architect.ForceComplete();
+                } else
+                {
+                    architect.Build(lines[Random.Range(0, lines.Length)]);
+
                 }
-                
-                architect.Build(lines[Random.Range(0, lines.Length)]);
             }
             else if (Input.GetKeyDown(KeyCode.A))
             {
